dbc2da262a3ae96c818976ab7e5c93109d747e7f
YAMANJEHAD <156370346+YAMANJEHAD@users.noreply.github.com>
2025-05-20 12:24:38 +0300

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -126,7 +126,7 @@ if uploaded_file:
         tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs([
             "📊 Note Type Summary",
             "👨‍🔧 Notes per Technician",
-            "🏆 Top 5 Technicians",
+            "🚨 Top 5 Technicians",
             "🥧 Note Type Distribution",
             "✅ DONE Terminals",
             "📑 Detailed Notes"
@@ -162,7 +162,7 @@ if uploaded_file:
             st.plotly_chart(fig)
 
         with tab5:
-            st.markdown("### ✅ Terminal IDs for 'DONE' Notes")
+            st.markdown("### ✅'DONE' Notes")
             done_terminals = df[df['Note_Type'] == 'DONE'][['Technician_Name', 'Terminal_Id', 'Ticket_Type']]
             done_terminals_counts = done_terminals['Technician_Name'].value_counts()
             done_terminals_table = done_terminals[done_terminals['Technician_Name'].isin(done_terminals_counts.head(5).index)]
