8cc3433936531b69a310cfbf00be122203f82874
YAMANJEHAD <156370346+YAMANJEHAD@users.noreply.github.com>
2025-05-28 12:09:13 +0300

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -12,7 +12,7 @@ import os
 import hashlib
 import re
 
-st.set_page_config(page_title="Note Analyzer", layout="wide")
+st.set_page_config(page_title="INTERSOFT Analyzer", layout="wide")
 
 clock_html = """<div style="background: transparent;"><style>
 .clock-container {
